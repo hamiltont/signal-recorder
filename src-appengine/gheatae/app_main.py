f323632931e85e74c825ea1425092f27fa4aadbe
Hamilton Turner <hamiltont@gmail.com>
2010-09-14 17:19:54 -0400

    Added the gheat-ae project to the repo. Provides heatmap tiles for Google Maps
    Full source of gheat-ae can be found at code.google.com/p/gheat-ae . I have
    not retained the .svn files here, and I do not plan on incrementally
    committing my changes back upstream. I would liked to have done this, but
    unfortunately my request for committer status on the project has not been
    answered, so I plan to work on the code base separately, and provide patch
    files if they approve of my changes. Also, the difficulty of setting up a svn
    project inside of a git project seemed a bit more than I wanted to manage
    for right now.
    
    Added git ignores for compiled python files. Working on integrating the
    gheat-ae project into my server code. The project is a bit outdated and
    works with a deprecated version of Google Maps. I will likely try to get
    that up and running before I try to upgrade anything serious.

---
+++ src-appengine/gheatae/app_main.py
@@ -0,0 +1,42 @@
+'''
+Created on Nov 11, 2009
+
+@author: ddelima
+
+@contributors
+    Hamilton Turner <hamiltont@gmail.com>
+    Added catch-all ability
+'''
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from gheatae import tile
+from gheatae import data
+
+class CatchAll(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("This is the catch all page. You probably don't want to be here<br /><br />")
+
+        self.response.out.write('<h3>Request URI</h3>')
+        self.response.out.write(self.request.uri)
+        self.response.out.write('<h3>Request Headers</h3>')
+        self.response.out.write(self.request.headers)
+        self.response.out.write('<h3>Request Body</h3>')
+        self.response.out.write(self.request.body)
+
+
+    def post(self):
+        self.get()
+
+
+application = webapp.WSGIApplication(
+   [('/gheat/tile/.*', tile.GetTile),
+    ('/gheat/data/.*', data.Data),
+    ('.*', CatchAll)],
+   debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
