1798b170a550f0d2e30c1b09bf717ce358075b64
Hamilton Turner <hamiltont@gmail.com>
2010-09-14 20:59:54 -0400

    Much better error messages and documentation. Also handles variable length URIs now

--- src-appengine/gheatae/tile.py
+++ src-appengine/gheatae/tile.py
@@ -6,6 +6,20 @@ import logging
 import time
 import handler
 
+__docs__  = \
+"""
+<html><body>
+Proper URI format is in the form
+<blockquote>www.host.com/path_to_retrieve_tiles/desired_layer/zoom_level/y-value,x-value.png</blockquote>
+For this server, an example would be
+<blockquote>signalrecorder.appspot.com/gheat/tile/classic/4/4,6.png</blockquote>
+You can always append ?docs=1 to the end of your URL to get the docs
+for this file.
+<br>TODO: Add in appropriate possibilities for each of the parameters
+<ul><li>zoom_level can be 0 <= zoom_level <= 30</li></ul>
+</html></body>
+"""
+
 log = logging.getLogger('tile')
 
 tile.cache = cache.Cache()
@@ -13,49 +27,84 @@ tile.provider = provider.DBProvider()
 
 class GetTile(handler.Handler):
 
-  def get(self):
-    log.info("Running GetTile:GET...")
-    st = time.clock()
-    path = environ['PATH_INFO']
+    def get(self):
+        if '1' == self.request.get('docs'):
+            self.response.out.write(__docs__)
+            return;
+        log.info("Running GetTile:GET...")
+        st = time.clock()
+        path = environ['PATH_INFO']
 
-    log.debug("Path:" + path)
-    if path.endswith('.png'):
-      raw = path[:-4] # strip extension
-      try:
-          assert raw.count('/') == 4, "%d /'s" % raw.count('/')
-          foo, bar, layer, zoom, yx = raw.split('/')
-#          assert color_scheme in color_schemes, ("bad color_scheme: "
-#                                                + color_scheme
-#                                                 )
-          assert yx.count(',') == 1, "%d /'s" % yx.count(',')
-          y, x = yx.split(',')
-          assert zoom.isdigit() and x.isdigit() and y.isdigit(), "not digits"
-          zoom = int(zoom)
-          x = int(x)
-          y = int(y)
-          assert 0 <= zoom <= (consts.MAX_ZOOM - 1), "bad zoom: %d" % zoom
-      except AssertionError, err:
-          log.error(err.args[0])
-          self.respondError(err)
-          return
-    else:
-      self.respondError("Invalid path")
-      return
+        log.debug("Path:" + path)
+        if False == path.endswith('.png'):
+            self.customError("Invalid path, needs to end with .png")
+            return
 
-#    color_scheme = color_schemes[color_scheme]
-    #try:
-    tile = Tile(layer, zoom, x, y)
-    log.info("Start-B1: %2.2f" % (time.clock() - st))
-#    except Exception, err:
-#      self.respondError(err)
-#      raise err
-#      return
+        raw = path[:-4] # everything except .png
+        try:
+            assert raw.count('/') >= 3, \
+                "%d slashes('/') found in '%s', need at least 3" % \
+                (raw.count('/'), raw)
 
-    self.response.headers['Content-Type'] = "image/png"
-    #log.info("Building image...")
-    img_data = tile.image_out()
-    log.info("Start-B2: %2.2f" % (time.clock() - st))
-    
-    #log.info("Writing out image...")
-    self.response.out.write(img_data)
-    log.info("Start-End: %2.2f" % (time.clock() - st))
+            """ TODO The layer is not being used right now (at all, not just here)
+                     I should try to figure out what it was intended for
+            """
+            layer, zoom, yx = raw.split('/')[-3:]
+#              assert color_scheme in color_schemes, ("bad color_scheme: "
+#                                                    + color_scheme
+#                                                     )
+            assert yx.count(',') == 1, "%d commas(',') found in '%s', \
+                can only have 1" % (yx.count(','), yx)
+
+            y, x = yx.split(',')
+
+            assert zoom.isdigit() and x.isdigit() and y.isdigit(), "One of \
+                these values is not a digit. All of them need to be digits.\
+                <br /> x: %s, y: %s, zoom_level: %s" % (x,y,zoom)
+
+            zoom = int(zoom)
+            x = int(x)
+            y = int(y)
+            assert 0 <= zoom <= (consts.MAX_ZOOM - 1), "Requested zoom_Level \
+                was: %d. This is not appropriate" % zoom
+
+        except AssertionError, err:
+            log.error(err.args[0])
+            self.customError(err)
+            return
+
+        #    color_scheme = color_schemes[color_scheme]
+        #try:
+        tile = Tile(layer, zoom, x, y)
+        log.info("Start-B1: %2.2f" % (time.clock() - st))
+        #    except Exception, err:
+        #      self.respondError(err)
+        #      raise err
+        #      return
+
+        self.response.headers['Content-Type'] = "image/png"
+
+        log.info("Building or retrieving image...")
+        img_data = tile.image_out()
+        log.info("Start-B2: %2.2f" % (time.clock() - st))
+
+        log.info("Writing out image...")
+        self.response.out.write(img_data)
+        log.info("Start-End: %2.2f" % (time.clock() - st))
+
+    def customError(self, message):
+        err = """<html><body>
+            <h2>Error</h2>
+            Proper URI format is in the form
+            <blockquote>www.host.com/path_to_retrieve_tiles/desired_layer/zoom_level/y-value,x-value.png</blockquote>
+            For this server, an example would be
+            <blockquote>signalrecorder.appspot.com/gheat/tile/classic/4/4,6.png</blockquote>
+            You can always append ?docs=1 to the end of your URL to get the docs
+            for this file. <br>See <a href='%s%s'>the docs for this page</a><br>
+            <br>TODO: Add in appropriate possibilities for each of the parameters
+            <ul><li>zoom_level can be %d <= zoom_level <= %d</li></ul>
+            <h3>Specific Error Message</h3>""" % (environ['PATH_INFO'], \
+                                                  '?docs=1',0, consts.MAX_ZOOM)
+        err =  err + str(message)
+        err += "</body></html>"
+        self.respondError(err)
