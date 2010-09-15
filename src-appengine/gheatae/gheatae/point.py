2f13586c12fbc79b9d5af3b2a260e779c606b0d3
Hamilton Turner <hamiltont@gmail.com>
2010-09-15 00:00:30 -0400

    Documentation update

--- src-appengine/gheatae/gheatae/point.py
+++ src-appengine/gheatae/gheatae/point.py
@@ -2,6 +2,11 @@ from gheatae.geo.geomodel import GeoModel
 from google.appengine.ext import db
 
 class DataPoint(GeoModel):
-  time = db.DateTimeProperty()
-  weight = db.IntegerProperty()
-  range = db.IntegerProperty()
+    """
+    Simple extension of GeoModel. This is mainly to separate the concerns of the
+    GeoModel from the concerns of the gheat library e.g GeoModel does not care
+    about time, weight, or range. Only gheat does
+    """
+    time = db.DateTimeProperty()
+    weight = db.IntegerProperty()
+    range = db.IntegerProperty()
